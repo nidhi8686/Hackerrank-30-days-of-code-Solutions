42535635ba6ab8177499314cc9abd3734db2c9ce
sourav santra <souravsantra59580@gmail.com>
2020-10-08 14:25:22 +0530

    day-6

---
+++ Day-6/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   public static void main(String[] args) 
+    {
+        Scanner scan1 = new Scanner(System.in);
+        int time =scan1.nextInt();
+        scan1.nextLine();
+        
+        while(time>0)
+            {
+            
+   
+        String str= scan1.nextLine();
+     
+        
+        char arr[] = str.toCharArray();
+        int len= str.length();
+        
+        for(int x=0;x<len;x=x+2)
+            {
+            
+            System.out.print(arr[x]);
+            
+            }
+        
+        System.out.print(" ");
+        
+         for(int x=1;x<len;x=x+2)
+            {
+            
+            System.out.print(arr[x]);
+         }
+        
+        System.out.println();
+        
+        time--;
+        }  
+    }
+}
